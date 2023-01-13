d710f3923f08398074fb226cf8419aeaa4db678c
darshan45672 <drshnbhandary@gmail.com>
2023-01-13 18:10:06 +0530

    updated data base

--- sql/blood_bank_database.sql
+++ sql/blood_bank_database.sql
@@ -24,7 +24,7 @@ Primary key(admin_id)
 
 /*  insert admin data into admin_info table*/
 insert into admin_info(admin_name,admin_username,admin_password)
-values("Varun","varunsardana004",123);
+values("Darshan","DarshanBhandary",1234567);
 
 /*create table blood in which all blood group is stored.*/
 create table blood(
