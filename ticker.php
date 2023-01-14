e891de6255f83bce701e19ea828134f2e66ec583
darshan45672 <drshnbhandary@gmail.com>
2023-01-14 19:20:16 +0530

    updated slider

--- ticker.php
+++ ticker.php
@@ -54,7 +54,7 @@
 </head>
 <body>
 <div class="news blue">
-<span style="background-color:#F60F0F;width:125px;height:50px">Latest Updates</span><span class="text1" ><marquee>First blood donation camp to be organised in A J Institute of Engineering and Technology By Darshan Bhandary in collabration with Blood Bank& Donation Management System on <b>16/01/2023 at Seminar Hall 3.</b> Come and Be a Part of this Noble Cause :) </marquee></span>
+<span style="background-color:#F60F0F;width:125px;height:50px">Latest Updates</span><span class="text1" ><marquee>First blood donation camp to b organised in A J Institute of Engineering and Technology By Darshan Bhandary in collabration with Blood Bank& Donation Management System on <b>16/01/2023 at Seminar Hall 3.</b> Come and Be a Part of this Noble Cause :) </marquee></span>
 </div>
 </body>
 </head>
